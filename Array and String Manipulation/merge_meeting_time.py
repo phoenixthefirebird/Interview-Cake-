4af3a35405da7d25e10b6ed16eb47650dd4ba284
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2020-08-29 12:43:41 -0700

    Create merge_meeting_time.py
    merge meeting

---
+++ Array and String Manipulation/merge_meeting_time.py
@@ -0,0 +1,21 @@
+  def merge_ranges(meetings):
+    # Sort by start time
+    sorted_meetings = sorted(meetings)
+
+    # Initialize merged_meetings with the earliest meeting
+    merged_meetings = [sorted_meetings[0]]
+
+    for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+        last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+        # If the current meeting overlaps with the last merged meeting, use the
+        # later end time of the two
+        if (current_meeting_start <= last_merged_meeting_end):
+            merged_meetings[-1] = (last_merged_meeting_start,
+                                   max(last_merged_meeting_end,
+                                       current_meeting_end))
+        else:
+            # Add the current meeting since it doesn't overlap
+            merged_meetings.append((current_meeting_start, current_meeting_end))
+
+    return merged_meetings
