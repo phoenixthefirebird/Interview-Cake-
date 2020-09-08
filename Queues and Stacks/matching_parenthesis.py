065a09a6834d8bc10841deb7bb6478049aa9135b
Phoenix Liu <55338489+phoenixthefirebird@users.noreply.github.com>
2020-09-07 22:09:17 -0700

    Create matching_parenthesis.py

---
+++ Queues and Stacks/matching_parenthesis.py
@@ -0,0 +1,15 @@
+def get_closing_paren(sentence, opening_paren_index):
+    open_nested_parens = 0
+
+    for position in range(opening_paren_index + 1, len(sentence)):
+        char = sentence[position]
+
+        if char == '(':
+            open_nested_parens += 1
+        elif char == ')':
+            if open_nested_parens == 0:
+                return position
+            else:
+                open_nested_parens -= 1
+
+    raise Exception("No closing parenthesis :(")
